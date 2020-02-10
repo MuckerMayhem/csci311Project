bb30e744728c8699269eeb77526cf5422b594c47
Cole <colehoul@gmail.com>
2020-02-10 13:28:41 -0800

    axios testing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response))
+  }
+}).$mount('app');
