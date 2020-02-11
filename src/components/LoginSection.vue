8b38dc873640fd94f6bd41278dbd115f0fe433c7
Ezra MacDonald <macdonaldezra@gmail.com>
2020-02-11 14:02:47 -0800

    Add router and configured basic file structure

---
+++ src/components/LoginSection.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container-fluid">
+    <div class="row no-gutter">
+      <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+      <div class="col-md-8 col-lg-6">
+        <div class="login d-flex align-items-center py-5">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-9 col-lg-8 mx-auto">
+                <h3 class="login-heading my-5 text-center">Welcome back!</h3>
+                <form>
+                  <div class="form-label-group">
+                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                  </div>
+
+                  <div class="form-label-group">
+                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                  </div>
+
+                  <div class="custom-control custom-checkbox mb-3">
+                    <input type="checkbox" class="custom-control-input" id="customCheck1">
+                    <label class="custom-control-label" for="customCheck1">Remember password</label>
+                  </div>
+                  <button class="btn btn-lg btn-dark btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Sign in</button>
+                  <div class="text-center">
+                    <a class="small" href="#">Forgot password?</a></div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginSection'
+}
+</script>
+
+<style scoped>
+
+.login, .image {
+  min-height: 100vh;
+}
+
+.bg-image {
+  /* Image retrieved from: https://www.flickr.com/photos/nasacommons/4857947689/ */
+  background-image: url('../assets/lunar-prospector.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+.login-heading {
+  font-weight: 300;
+}
+
+.btn-login {
+  font-size: 0.9rem;
+  letter-spacing: 0.05rem;
+  padding: 0.75rem 1rem;
+  border-radius: 2rem;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group > input,
+.form-label-group > label {
+  height: auto;
+  border-radius: 2rem;
+}
+
+.form-label-group > label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  cursor: text;
+  /* Match the input under the label */
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  transition: all .1s ease-in-out;
+}
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="register">
+    <RegisterSection/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RegisterSection from '@/components/RegisterSection.vue'
+
+
+export default {
+  name: 'Register',
+  components: {
+    RegisterSection
+  }
+}
+</script>
