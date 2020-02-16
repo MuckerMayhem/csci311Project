b232eb96604b655a1d3bf51831b30c8f682e0a42
Cole <continumconstil@gmail.com>
2020-02-16 11:40:22 -0800

    Revert "Merge branch 'ezra' into cole"
    This reverts commit 76e9b02284f9f29945e82bd645e0568df801546c.

--- src/router/index.js
+++ src/router/index.js
@@ -1,112 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuelidate from 'vuelidate'
-import VueCookies from 'vue-cookies'
 
-import Login from '@/views/Login.vue'
-import About from '@/views/About.vue'
-import Register from '@/views/Register.vue'
-import Home from '@/views/Home.vue'
-
+import Login from '../views/Login.vue'
+import About from '../views/About.vue'
+import Register from '../views/Register.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 Vue.use(Vuelidate)
-Vue.use(VueCookies)
-
-Vue.$cookies.config('1d')
-
-
-const ifNotAuthenticated = (to, from, next) => {
-  if (!Vue.$cookies.isKey("session_id")) {
-    next()
-    return
-  }
-  next('/')
-}
-
-const ifAuthenticated = (to, from, next) => {
-  if (Vue.$cookies.isKey("session_id")) {
-    next()
-    return
-  }
-  next('/login')
-}
 
 const routes = [
   {
     path: '/login',
     name: 'Login',
-    meta: {
-      title: 'Login - NASAnalysis',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The page to log in to NASAnalysis to access your account.'
-        },
-        {
-          name: 'og:description',
-          content: 'The page to log in to NASAnalysis to access your account.'
-        }
-      ]
-    },
-    component: Login,
-    beforeEnter: ifNotAuthenticated
+    component: Login
   },
   {
     path: '/about',
     name: 'About',
-    meta: {
-      title: 'About - NASAnalysis',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The page to learn more about the purpose of NASAnalysis and its history.'
-        },
-        {
-          name: 'og:description',
-          content: 'The page to learn more about the purpose of NASAnalysis and its history.'
-        }
-      ]
-    },
-    component: About,
-    beforeEnter: ifNotAuthenticated
+    component: About
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/register',
     name: 'Register',
-    meta: {
-      title: 'Register - NASAnalysis',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The page to register for NASAnalysis'
-        },
-        {
-          name: 'og:description',
-          content: 'The page to register for NASAnalysis'
-        }
-      ]
-    },
-    component: Register,
+    component: Register
   },
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    meta: {
-      title: 'Home - NASAnalysis',
-      metaTags: [
-        {
-          name: 'description',
-          content: 'The page to access the information presented by NASAnalysis and learn more about NASA\'s datasets!'
-        },
-        {
-          name: 'og:description',
-          content: 'The page to access the information presented by NASAnalysis and learn more about NASA\'s datasets!'
-        }
-      ]
-    },
-    component: Home,
-    beforeEnter: ifAuthenticated
+    component: Home
   }
 
 ]
@@ -117,48 +44,5 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-
-// router.beforeEach((to, from, next) => {
-//   if ((from === "/login" || "/register") && (Vue.$cookies.isKey("session_id"))) next('/')
-//   else if ((from === "") Vue.$cookies.isKey("session_id")) next("/")
-// });
-
-// Below Router Function obtained from article found at the following link:
-// https://alligator.io/vuejs/vue-router-modify-head/
-
-// This callback runs before every route change, including on page load.
-router.beforeEach((to, from, next) => {
-  // This goes through the matched routes from last to first, finding the closest route with a title.
-  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
-  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
-
-  // Find the nearest route element with meta tags.
-  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-
-  // If a route with a title was found, set the document (page) title to that value.
-  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
-
-  // Remove any stale meta tags from the document using the key attribute we set below.
-  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
-
-  // Skip rendering meta tags if there are none.
-  if(!nearestWithMeta) return next();
-
-  // Turn the meta tag definitions into actual elements in the head.
-  nearestWithMeta.meta.metaTags.map(tagDef => {
-    const tag = document.createElement('meta');
-
-    Object.keys(tagDef).forEach(key => {
-      tag.setAttribute(key, tagDef[key]);
-    });
-
-    // We use this to track which meta tags we create, so we don't interfere with other ones.
-    tag.setAttribute('data-vue-router-controlled', '');
-
-    return tag;
-  }).forEach(tag => document.head.appendChild(tag));
-
-  next();
-});
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,15 @@
 <template>
   <div class="home">
-    <h2>Picture of the Day</h2>
-    <div class="container">
-      <img class="picOfTheDay" v-bind:src="image" v-bind:alt="title"/>
-      <h4 class="py-3">{{ title }}</h4>
-      <p class="picOfDayDescription mx-auto">{{ description }}</p>
-    </div>
-    
+    <h1>Data Presentation Information</h1>
+    <h4>
+      It goes here!
+    </h4>
   </div>
 </template>
-
-<script>
-  import axios from 'axios';
-  import { NASA_PICTURE_OF_DAY_QUERY } from "../../instance/config.js"
-
-  export default {
-    name: 'Home',
-
-    data () {
-      return {
-        image: null,
-        title: null,
-        description: null
-      }
-    },
-
-    mounted() {
-      axios.get(NASA_PICTURE_OF_DAY_QUERY).then(response => (
-        this.image = response.data.url,
-        this.title = response.data.title,
-        this.description = response.data.explanation
-      ))
-    }
-  }
-</script>
 
 <style scoped>
 .home {
   min-height: 100vh;
   margin: auto;
-}
-
-.picOfTheDay {
-  max-height: 50vh;
-  padding-top: 7.5rem;
-}
-
-.picOfDayDescription {
-  width: 75%;
-  line-height: 1.25;
-  text-align: justify;
 }
 </style>
