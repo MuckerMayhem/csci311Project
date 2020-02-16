a5d5050fe4b9149b65a869abe85f97f2c42c3a1e
Ezra MacDonald <macdonaldezra@gmail.com>
2020-02-16 11:13:25 -0800

    - Add minimal authentication flow for routing \n- Add Registration form component \n- Add page descriptions and title \n- Add home page with picture of the day

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light static-top shadow navbarHeight">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow navbarHeight">
       <div class="container">
         <router-link to="/login" class="nav-link navbar-brand text-dark">
-          CSCI311 Team Project
+          NASAnalysis
         </router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" 
           data-target="#navbarResponsive" aria-controls="navbarResponsive" 
           aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item px-2">
-              <router-link to="/login" class="nav-link">Home</router-link>
-            </li>
-            <li class="nav-item px-2">
-              <router-link to="/about" class="nav-link">About</router-link>
-            </li>
-            <li class="nav-item px-2">
-              <router-link to="/register" class="nav-link">Register</router-link>
-            </li>
+            <template v-if="authenticated === true">
+              <li class="nav-item px-2">
+                <button class="nav-item px-2 btn text-dark" @click="logout()">Logout</button>
+              </li>
+            </template>
+            <template v-if="authenticated === false">
+              <li class="nav-item px-2">
+                <router-link to="/about" class="nav-link">About</router-link>
+              </li>
+              <li class="nav-item px-2">
+                <router-link to="/login" class="nav-link">Login</router-link>
+              </li>
+              <li class="nav-item px-2">
+                <router-link to="/register" class="nav-link">Register</router-link>
+              </li>
+            </template>
           </ul>
         </div>
       </div>
@@ -29,6 +37,28 @@
   </div>
 </template>
 
+<script>
+
+  export default {
+    data() {
+      return {
+        authenticated: false
+      }
+    },
+    watch: {
+      $route () {
+        this.authenticated = this.$cookies.isKey("session_id")
+      }
+    },
+    methods: {
+      logout() {
+        this.$cookies.remove("session_id");
+        this.$router.push("/login");
+      }
+    }
+
+  }
+</script>
 
 <style>
 #app {
@@ -45,4 +75,6 @@
 .navbarHeight {
   height: 5rem;
 }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 import router from './router'
 
+// var store = {
+//   state: {
+//     authenticated: false,
+//   },
+
+//   isLoggedIn() {
+//     if (this.$cookies.isKey("session_id")) {
+//       this.state.authenticated = true;
+//     } else {
+//       this.state.authenticated = false;
+//     }
+//   }
+// }
 
 Vue.config.productionTip = false
 
 new Vue({
+  // data: store,
   router,
   render: h => h(App)
 }).$mount('#app')
